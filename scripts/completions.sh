f4592b05bc00663778b115bab722c3dfc11d9344
Drew Stinnett <drew@drewlink.com>
2023-10-07 16:53:19 -0400

    Fixing completion

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -3,5 +3,5 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
-    go run main.go completion "$sh" >"completions/taskpoet.$sh"
+    go run ./cli completion "$sh" >"completions/taskpoet.$sh"
 done
